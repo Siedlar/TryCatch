c819c66e4c887730e694ab8126c2c2571126ff4b
Siedlar <ksiedlarski@gmail.com>
2020-03-27 12:50:18 +0100

    dodanie do/while

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
     Scanner in=new Scanner(System.in);
+    int c=1;
+    do{
     try{
         System.out.println("Podaj 1 numer");
         int a=in.nextInt();
-        System.out.println("Podaj 1 numer");
+        System.out.println("Podaj 2 numer");
         int b=in.nextInt();
         int suma=a/b;
+      c=2;
         System.out.println(suma);}
     catch(ArithmeticException e){
-
-    }
+        System.out.println("Podales złą liczbę");
+    }}while(c==1);
         System.out.println("essasiso");
     }
 }
